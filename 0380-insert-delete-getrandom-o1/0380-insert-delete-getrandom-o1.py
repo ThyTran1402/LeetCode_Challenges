d04508507cacbbf6b8509dca86bef21ffb70d4ce
Thy Tran <58045538+ThyTran1402@users.noreply.github.com>
2022-11-28 21:05:18 -0500

    Time: 1036 ms (52.01%), Space: 61 MB (97.25%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,26 +1,30 @@
-import random
+from random import choice
+
 class RandomizedSet:
 
     def __init__(self):
-        self.numbers = []
-        self.positions = {}
+        self.index = {} #hashMap
+        self.store = [] #array
 
     def insert(self, val: int) -> bool:
-        if val in self.positions:
+        if val in self.index:
             return False
-        self.positions[val] = len(self.numbers)
-        self.numbers.append(val) #insert a value into the set
-        return True # if the set did not contain specified element
+        #insert the actual value as a key and its index as a value
+        self.index[val] = len(self.store)
+        #append new value to array
+        self.store.append(val)
+        return True
     
 #Time complexity: O(1)
 #Space complexity: O(N) -> N is number of element in the array
 
     def remove(self, val: int) -> bool:
-        if val in self.positions:
-            lastElement, idx = self.numbers[-1], self.positions[val]
-            self.numbers[idx], self.positions[lastElement] = lastElement, idx
-            self.numbers.pop() #delete the last element
-            del self.positions[val]
+        if val in self.index:
+            last, i = self.store[-1], self.index[val]
+            self.store[i], self.index[last] = last, i
+            #delete the last element
+            del self.index[val]
+            self.store.pop()
             return True
         return False
     
@@ -30,7 +34,7 @@ class RandomizedSet:
     def getRandom(self) -> int:
         #get random element from the set
         
-        return self.numbers[random.randint(0, len(self.numbers)-1)]
+        return choice(self.store)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
